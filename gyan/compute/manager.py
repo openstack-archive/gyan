80839071014577f1546476a1726ef592b21a9239
bharath <bharath_ves@hotmail.com>
2018-10-14 00:22:56 +0530

    Add tensorflow driver implementation
    Change-Id: I951cea9325d2ea4a843ea55d1731c481df899474

--- gyan/compute/manager.py
+++ gyan/compute/manager.py
@@ -12,7 +12,9 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
+import base64
 import itertools
+import os
 
 import six
 import time
@@ -49,17 +51,18 @@ class Manager(periodic_task.PeriodicTasks):
         self.host = CONF.compute.host
         self._resource_tracker = None
 
-    def ml_model_create(self, context, limits, requested_networks,
-                         requested_volumes, ml_model, run, pci_requests=None):
-        @utils.synchronized(ml_model.uuid)
-        def do_ml_model_create():
-            created_ml_model = self._do_ml_model_create(
-                context, ml_model, requested_networks, requested_volumes,
-                pci_requests, limits)
-            if run:
-                self._do_ml_model_start(context, created_ml_model)
+    def ml_model_create(self, context, ml_model):
+        db_ml_model = objects.ML_Model.get_by_uuid_db(context, ml_model["id"])
+        utils.save_model(CONF.state_path, db_ml_model)
+        obj_ml_model = objects.ML_Model.get_by_uuid(context, ml_model["id"])
+        obj_ml_model.status = consts.SCHEDULED
+        obj_ml_model.status_reason = "The ML Model is scheduled and saved to the host %s" % self.host
+        obj_ml_model.save(context)
 
-        utils.spawn_n(do_ml_model_create)
+    def ml_model_predict(self, context, ml_model_id, kwargs):
+        #open("/home/bharath/Documents/0.png", "wb").write(base64.b64decode(kwargs["data"]))
+        model_path = os.path.join(CONF.state_path, ml_model_id)
+        return self.driver.predict(context, model_path, base64.b64decode(kwargs["data"]))
 
     @wrap_ml_model_event(prefix='compute')
     def _do_ml_model_create(self, context, ml_model, requested_networks,
